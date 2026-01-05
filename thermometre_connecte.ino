2678bb05b5b97434d31d36942df93c91a8370749
Florian-DAUVERGNE <florian.dauv@gmail.com>
2026-01-05 18:56:18 +0100

    feat: included all new features

--- thermometre_connecte.ino
+++ thermometre_connecte.ino
@@ -8,16 +8,19 @@
 #define BROCHE_DHT 10
 #define TYPE_DHT DHT22
 
+#define BUTTON_PIN 3
+
 CapteurDHT monDHT(BROCHE_DHT, TYPE_DHT);
 
 TempDisplay tempDisp(CLK, DIO);
 
 // Adresse MAC du récepteur
-uint8_t receiverMac[] = {0x1C, 0xDB, 0xD4, 0x3C, 0x75, 0xC8};
+uint8_t receiverMac[] = { 0x1C, 0xDB, 0xD4, 0x3C, 0x75, 0xC8 };
 EspNowEmitter emitter(receiverMac);
 
 void setup() {
   Serial.begin(115200);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   emitter.begin();
   monDHT.begin();
   tempDisp.begin();  // initialisation de l'afficheur
@@ -25,13 +28,23 @@ void setup() {
 
 void loop() {
   float humidite, temperature, temperatureRessentie;
+  int etat = digitalRead(BUTTON_PIN);
 
-  if (monDHT.lire(humidite, temperature, temperatureRessentie)) {
-    //monDHT.afficher(humidite, temperature, temperatureRessentie);
-    tempDisp.showTemperature(temperature);
-    emitter.sendInt(temperature);
-  } else {
-    tempDisp.showTemperature(230);
+
+  if (!monDHT.lire(humidite, temperature, temperatureRessentie)) {
+    tempDisp.showError();
+    delay(1000);
+    return;
   }
-  delay(2000);
+
+  if (etat == LOW) {
+    tempDisp.showHumidity(humidite);
+    emitter.sendInt(temperatureRessentie,humidite);
+    delay(500);
+    return;
+  }
+
+  tempDisp.showTemperature(temperatureRessentie);
+  emitter.sendInt(temperatureRessentie,humidite);
+  delay(500);
 }
