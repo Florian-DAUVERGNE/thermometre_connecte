8fceb5799c304cf84f1e635aa3e1d6d4d7229796
Florian-DAUVERGNE <florian.dauv@gmail.com>
2026-01-05 18:53:55 +0100

    feat: added led blinkink on receive

--- lib/espnow_receiver.h
+++ lib/espnow_receiver.h
@@ -9,20 +9,7 @@ class EspNowReceiver {
 private:
     message data;
 
-    static void onReceive(const esp_now_recv_info_t *info, const uint8_t *incomingData, int len) {
-        message received;
-        memcpy(&received, incomingData, sizeof(received));
-        Serial.print("Température reçue : ");
-        Serial.println(received.temperature);
-
-        // Optionnel : afficher MAC de l'émetteur
-        char macStr[18];
-        snprintf(macStr, sizeof(macStr),
-                 "%02X:%02X:%02X:%02X:%02X:%02X",
-                 info->src_addr[0], info->src_addr[1], info->src_addr[2],
-                 info->src_addr[3], info->src_addr[4], info->src_addr[5]);
-        Serial.print("De : "); Serial.println(macStr);
-    }
+    ledFlash = true;
 
 public:
     void begin() {
--- recepteur_esp_now/recepteur_esp_now.ino
+++ recepteur_esp_now/recepteur_esp_now.ino
@@ -1,12 +1,24 @@
 #include "../lib/espnow_receiver.h"
 
+
+volatile bool EspNowReceiver::ledFlash = false;
+message EspNowReceiver::received;
+
 EspNowReceiver receiver;
+
 
 void setup() {
     Serial.begin(115200);
+    pinMode(8, OUTPUT);
     receiver.begin();
 }
 
 void loop() {
-    // Rien à faire ici, tout est géré par le callback ESP-NOW
+  if (EspNowReceiver::ledFlash) {
+    digitalWrite(8, LOW);   // ON
+    delay(500);
+    digitalWrite(8, HIGH);  // OFF
+    delay(500);
+    EspNowReceiver::ledFlash = false;
+  }
 }
