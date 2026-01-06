c5f39d7ecb66b2eb474b9d19cb541dba079522e9
Florian-DAUVERGNE <florian.dauv@gmail.com>
2026-01-06 18:25:45 +0100

    feat: made code for connected relay

---
+++ relais_connecte/relais_connecte.ino
@@ -0,0 +1,48 @@
+#include "../lib/espnow_receiver.h"
+
+#define RELAIS 6
+#define LED 8
+
+#define TEMP_ON  20.5
+#define TEMP_OFF 21.5
+
+volatile bool EspNowReceiver::ledFlash = false;
+message EspNowReceiver::received;
+
+EspNowReceiver receiver;
+
+bool relaisEtat = false;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+  pinMode(RELAIS, OUTPUT);
+  receiver.begin();
+}
+void loop() {
+  if (EspNowReceiver::ledFlash) {
+
+    message msg = EspNowReceiver::getReceived();
+
+    Serial.print("Température : ");
+    Serial.print(msg.temperature, 2);
+    Serial.print(" °C | Humidité : ");
+    Serial.print(msg.humidite, 1);
+    Serial.println(" %");
+
+    digitalWrite(LED, LOW);
+    delay(500);
+    digitalWrite(LED, HIGH);
+    delay(500);
+
+    EspNowReceiver::ledFlash = false;
+
+    if (!relaisEtat && msg.temperature > TEMP_OFF) {
+      relaisEtat = true;
+      digitalWrite(RELAIS, LOW);
+    } else if (relaisEtat && msg.temperature < TEMP_ON) {
+      relaisEtat = false;
+      digitalWrite(RELAIS, HIGH);
+    }
+  }
+}
