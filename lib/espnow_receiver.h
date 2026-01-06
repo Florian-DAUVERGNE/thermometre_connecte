404799c9c73a8db92a78aae3fec1ab2466238960
Florian-DAUVERGNE <florian.dauv@gmail.com>
2026-01-06 12:30:39 +0100

    style: formated espnow_receiver.h

--- lib/espnow_receiver.h
+++ lib/espnow_receiver.h
@@ -5,40 +5,43 @@
 #include <esp_now.h>
 #include "message.h"
 
-class EspNowReceiver {
+class EspNowReceiver
+{
 public:
     static volatile bool ledFlash;
     static message received;
 
-static void onReceive(const esp_now_recv_info_t *info,
-                      const uint8_t *incomingData,
-                      int len)
-{
-    if (len < sizeof(message)) return;
+    static void onReceive(const esp_now_recv_info_t *info,
+                          const uint8_t *incomingData,
+                          int len)
+    {
+        if (len < sizeof(message))
+            return;
 
-    memcpy(&received, incomingData, sizeof(message));
+        memcpy(&received, incomingData, sizeof(message));
 
-    Serial.print("Température : ");
-    Serial.print(received.temperature, 2);
-    Serial.print(" °C | Humidité : ");
-    Serial.print(received.humidite, 1);
-    Serial.println(" %");
+        Serial.print("Température : ");
+        Serial.print(received.temperature, 2);
+        Serial.print(" °C | Humidité : ");
+        Serial.print(received.humidite, 1);
+        Serial.println(" %");
 
-    char macStr[18];
-    snprintf(macStr, sizeof(macStr),
-             "%02X:%02X:%02X:%02X:%02X:%02X",
-             info->src_addr[0], info->src_addr[1], info->src_addr[2],
-             info->src_addr[3], info->src_addr[4], info->src_addr[5]);
+        char macStr[18];
+        snprintf(macStr, sizeof(macStr),
+                 "%02X:%02X:%02X:%02X:%02X:%02X",
+                 info->src_addr[0], info->src_addr[1], info->src_addr[2],
+                 info->src_addr[3], info->src_addr[4], info->src_addr[5]);
 
-    Serial.print("De : ");
-    Serial.println(macStr);
-    ledFlash = true;
-}
+        Serial.print("De : ");
+        Serial.println(macStr);
+        ledFlash = true;
+    }
 
-
-    void begin() {
+    void begin()
+    {
         WiFi.mode(WIFI_STA);
-        if (esp_now_init() != ESP_OK) {
+        if (esp_now_init() != ESP_OK)
+        {
             Serial.println("Erreur ESP-NOW");
             return;
         }
